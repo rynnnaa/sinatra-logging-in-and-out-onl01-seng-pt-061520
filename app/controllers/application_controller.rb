18c10f66bae804c64676d1e73d1b3805c7874a86
Ryna Truong <rynatruong@outlook.com>
2020-09-30 16:29:53 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,35 +1,36 @@
 require_relative '../../config/environment'
 class ApplicationController < Sinatra::Base
-  
   configure do
     set :views, Proc.new { File.join(root, "../views/") }
     enable :sessions unless test?
     set :session_secret, "secret"
   end
 
-
   get '/' do
     erb :index
   end
 
   post '/login' do
-    @user = User.find_by(username: params[:username], password: params[:password])
-    if @user
+    @user = User.find_by(:username => params[:username])
+    if @user != nil && @user.password == params[:password]
       session[:user_id] = @user.id
       redirect to '/account'
+    end
+    erb :error
+  end
+
+  get '/account' do
+    @current_user = User.find_by_id(session[:user_id])
+    if @current_user
+      erb :account
     else
       erb :error
     end
   end
 
-  get '/account' do
-    erb :account
-  end
-
   get '/logout' do
-    session[:user_id] = nil
-    redirect to '/'
-
+    session.clear
+    redirect '/'
   end
 
 
